a1301785bc36d2921f419278b38c97bb1ecc93f3
柏霜 <wenle.bwl@alibaba-inc.com>
2024-06-19 19:04:50 +0800

    fix app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import time
+
+import redis
+from flask import Flask
+
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
@@ -16,4 +21,3 @@ def get_hit_count():
 def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
-
